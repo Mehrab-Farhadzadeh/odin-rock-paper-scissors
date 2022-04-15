9ad276a0b1486f57cec0f74039202b2e3920fde2
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-04-16 03:11:27 +0430

    Add whoWins function
    Determines the winner and returns a proper message

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,3 +3,24 @@ function computerPlay() {
   let randomChoice = Math.floor(Math.random() * 3);
   return options[randomChoice];
 }
+
+function whoWins(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Tie, Both are "${playerSelection}"`;
+  }
+  if (playerSelection === "Rock") {
+    return computerSelection === "Paper"
+      ? `You Lose! "${computerSelection}" beats "${playerSelection}".`
+      : `You Win! "${playerSelection}" beats "${computerSelection}".`;
+  }
+  if (playerSelection === "Paper") {
+    return computerSelection === "Scissors"
+      ? `You Lose! "${computerSelection}" beats "${playerSelection}".`
+      : `You Win! "${playerSelection}" beats "${computerSelection}".`;
+  }
+  if (playerSelection === "Scissors") {
+    return computerSelection === "Rock"
+      ? `You Lose! "${computerSelection}" beats "${playerSelection}".`
+      : `You Win! "${playerSelection}" beats "${computerSelection}".`;
+  }
+}
